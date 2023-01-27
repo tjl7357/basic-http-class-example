2ceb38adf26d9379eb62ca681fcc4dd4b07a0daa
TJ Larson <tjl7357@rit.edu>
2023-01-27 14:30:39 -0500

    My First Webpage Made

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const http = require("http");
+const fs = require("fs");
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+
+const onRequest = (request, response) => {
+    response.writeHead(200, {
+        "Content-Type" : "audio/mpeg"
+    });
+
+    response.write(index);
+    response.end();
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
