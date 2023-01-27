95a65c5193f5982a828693463b6d1aab30b3630e
TJ Larson <tjl7357@rit.edu>
2023-01-27 14:37:34 -0500

    Removed console.log

--- src/server.js
+++ src/server.js
@@ -15,5 +15,5 @@ const onRequest = (request, response) => {
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+    //console.log(`Server running on port ${port}.`);
 });
